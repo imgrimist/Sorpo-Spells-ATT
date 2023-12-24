d6898aa4f2944d34d27765fbf9994c5ed6b53d5e
imgrimist <134544021+imgrimist@users.noreply.github.com>
2023-12-24 03:27:56 -0800

    Create UserLogin.js

---
+++ UserLogin.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // NOTE: To get a client secret/ID please email `support@altavr.io`. In that email include your in-game name and what you would like your bot to be called.
+  attConfig: {
+    clientId: 'Client ID goes inside here',
+    clientSecret: 'Client Secret goes inside here',
+    scope: [ 'ws.group', 'ws.group_members', 'ws.group_servers', 'ws.group_bans', 'ws.group_invites', 'group.info', 'group.join', 'group.leave', 'group.view', 'group.members', 'group.invite', 'server.view', 'server.console']
+  }
+} 
